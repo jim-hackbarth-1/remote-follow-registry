05338377693690722ce139af6ee1d40b755b514f
jim-hackbarth-1 <164082662+jim-hackbarth-1@users.noreply.github.com>
2024-03-19 16:56:52 -0500

    add RegistryDatabase project

---
+++ Implementation/Data/RegistryDatabase/Authentication/Account.sql
@@ -0,0 +1,12 @@
+﻿create table [Authentication].[Account]
+(
+	[Id] int identity(1, 1) not null,
+	AccountName nvarchar(256) not null,
+	PasswordHash nvarchar(50) not null,
+	PasswordSalt nvarchar(50) not null,
+	constraint [Authentication_Account_PK] primary key clustered ([Id] asc)
+)
+
+go
+create unique nonclustered index [Authentication_Account_AccountName_IX]
+	on [Authentication].[Account]([AccountName] asc) with (fillfactor = 90);
